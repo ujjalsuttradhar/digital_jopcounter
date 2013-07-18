24c03eee778a38fc4390537d6802410ac3a0b648
Ujjal Suttra Dhar <usuttra@yahoo.com>
2013-07-18 09:21:33 +0600

    target is finished for now. future work : Alarm when a mala is finished

--- src/digital_jopcounter/gui.java
+++ src/digital_jopcounter/gui.java
@@ -6,22 +6,21 @@ import java.io.IOException;
 import javax.swing.*;
 
 /**
- * gui.java is responsible for user interface Developped by JAVA SWING
- * @author Ujjal Suttra Dhar
+ * gui.java is responsible for user interface using JAVA SWING
+ * @author Ujjal Suttra Dhar <ujjalsuttradhar@gmail.com>
  */
 public class gui {
 
     JFrame frm;
     JButton reset, count, start, exit;
-    JLabel ponchototto,mohamontro,ponchototto1,mohamontro1,mc,cc;
-    final String pnc    = "Jaya Sri-Krishna-Chaitanya\nPrabhu Nityananda";
-    final String pnc1   = "Sri-Adwaita Gadadhara Shrivasadi-Gaura-Bhakta-Vrinda";
-    final String mhmnt  = "Hare Krishna Hare Krishna Krishna Krishna Hare Hare";
-    final String mhmnt1 = "Hare Rama Hara Rama Rama Rama Hare Hare।";
+    JLabel ponchototto,mohamontro,mc,cc;
     int counter = 0,mcounter=0;
 
+    /**
+     * setStatus() changes the status portion of the UI according to current value of counters
+     */
     private void setStatus(){
-       mc.setText(mcounter+" টি মালা জপ শেষ।");
+                mc.setText(mcounter+" টি মালা জপ শেষ।");
                 cc.setText(counter+" বার জপ শেষ।");
             
     
@@ -39,60 +38,49 @@ public class gui {
         } catch (Exception e) {
         }
 
+        
         start = new JButton("Start");
+        frm.add(start);
+        start.setBounds(10, 10, 100, 25);
+       
+        
 
         count = new JButton("Count");
         count.setVisible(false);
-
+        frm.add(count);
+        count.setBounds(10, 260, 100, 25);
+        
         reset = new JButton("Reset");
         reset.setVisible(false);
-
+        frm.add(reset);
+        reset.setBounds(200, 260, 100, 25);
+       
         exit = new JButton("Exit");
+        frm.add(exit);
+        exit.setBounds(400, 260, 100, 25);
 
         ponchototto = new JLabel(new ImageIcon("ponchototto.png"));
         ponchototto.setVisible(false);
-
+        frm.add(ponchototto);
+        ponchototto.setBounds(0, 0, 250, 88);
+          
         mohamontro = new JLabel(new ImageIcon("mohamontro.png"));
         mohamontro.setVisible(false);
-
-        ponchototto1 = new JLabel(pnc1);
-        ponchototto1.setVisible(false);
-
-        mohamontro1 = new JLabel(mhmnt1);
-        mohamontro1.setVisible(false);
+        frm.add(mohamontro);
+        mohamontro.setBounds(1, 170, 220, 88);
         
         mc = new JLabel("");
         mc.setVisible(false);
-        
+        frm.add(mc);
+        mc.setBounds(35, 120, 400, 15);
+       
         cc = new JLabel("");
         cc.setVisible(false);
-        
-        frm.add(start);
-        frm.add(count);
-        frm.add(reset);
-        frm.add(ponchototto);
-        //frm.add(ponchototto1);
-        frm.add(mohamontro);
-       // frm.add(mohamontro1);
-        frm.add(mc);
         frm.add(cc);
-        
-        frm.add(exit);
-
-
-        start.setBounds(10, 10, 100, 25);
-        ponchototto.setBounds(0, 0, 250, 88);
-      //  ponchototto1.setBounds(5, 30, 400, 25);
-        mc.setBounds(35, 120, 400, 15);
         cc.setBounds(35, 140 , 400, 15);
         
-        count.setBounds(10, 260, 100, 25);
         
-        mohamontro.setBounds(1, 170, 220, 88);
-       // mohamontro1.setBounds(10, 200, 400, 25);
-        reset.setBounds(200, 260, 100, 25);
-        exit.setBounds(400, 260, 100, 25);
-
+        
 
 
         /*Actionlistenet for start button*/
@@ -100,7 +88,6 @@ public class gui {
             public void actionPerformed(ActionEvent e) {
                 counter = 0;
                 ponchototto.setVisible(true);
-                ponchototto1.setVisible(true);
                 count.setText("Count (0)");
                 count.setVisible(true);
                 start.setVisible(false);
@@ -117,22 +104,7 @@ public class gui {
         reset.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 
-                
-                
                 counter  = 0;
-                //mcounter = 0;
-                
-                //ponchototto.setVisible(false);
-                //ponchototto1.setVisible(false);
-                
-                //mohamontro.setVisible(false);
-                //mohamontro1.setVisible(false);
-                
-                //count.setText("Count (0)");
-                //count.setVisible(false);
-                
-                //start.setVisible(true);
-                //reset.setVisible(false);
                 count.setText("Count (0)");
                 setStatus();
              }
@@ -144,8 +116,6 @@ public class gui {
             public void actionPerformed(ActionEvent e) {
                 counter++;
 
-                
-                
                 if (counter >= 108) {
                     mcounter++;
                     counter=0;
@@ -157,9 +127,6 @@ public class gui {
                 count.setText("Count (" + counter + ")");
                 ponchototto.setVisible(true);
                 mohamontro.setVisible(true);
-                ponchototto1.setVisible(true);
-                mohamontro1.setVisible(true);
-
             }
         });
 
@@ -174,5 +141,6 @@ public class gui {
 
         frm.setVisible(true);
         frm.setLocationRelativeTo(null);
+        frm.setResizable(false);
     }
 } //end of class
